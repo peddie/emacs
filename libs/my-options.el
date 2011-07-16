4d740aa0b25434d245368549bf264dd498ac1935
Matthew Peddie <mpeddie@gmail.com>
2011-07-16 04:38:24 -0700

    A few changes:
    * Moved the last of emacs configuration from Xresources into elisp
    * Finally got solarized theme working correctly in Xresources, since I
    wanted emacsclient -t to integrate properly with rxvt-unicode and
    mutt.  This led to a few more changes in the emacs theme switching.

--- libs/my-options.el
+++ libs/my-options.el
@@ -2,16 +2,19 @@
 (local-path "site-lisp/solarized")
 (require 'color-theme-solarized)
 
-(global-font-lock-mode 1)
-(menu-bar-mode 0)
-(scroll-bar-mode 0)
-(tool-bar-mode 0)
-(auto-compression-mode 1)
-(setq inhibit-startup-message t)
-(setq inhib)
-(setq mouse-yank-at-point t)
-(setq font-lock-maximum-decoration t)
-(fset 'yes-or-no-p 'y-or-n-p)
+
+(defun reasonable-settings ()
+  (progn
+    (global-font-lock-mode 1)
+    (menu-bar-mode 0)
+    (scroll-bar-mode 0)
+    (tool-bar-mode 0)
+    (auto-compression-mode 1)
+    (setq inhibit-startup-message t)
+    (setq inhib)
+    (setq mouse-yank-at-point t)
+    (setq font-lock-maximum-decoration t)
+    (fset 'yes-or-no-p 'y-or-n-p)))
 
 ;; scrolling (from http://www.djcbsoftware.nl/dot-emacs.html)
 (setq 
@@ -30,15 +33,27 @@
 ;; 	(background-color . "grey85") 
 ;; 	(foreground-color . "grey7") 
 
-(color-theme-solarized-light)
-(setq solarized-termcolors 256)
-(setq solarized-contrast 'normal)
+;; (setq solarized-termcolors 256)
+;; (setq solarized-contrast 'normal)
+;; (color-theme-solarized-light)
+;; (setq color-theme-is-global nil)
 
 (setq default-frame-alist
- 	'((frame-title-format . "emacs - %b")))
+ 	'((frame-title-format . "emacs - %b")
+	  (menu-bar-mode . 0)
+	  (scroll-bar-mode . 0)
+	  (tool-bar-mode . 0)))
 
-(add-hook 'window-setup-hook
-	  '(lambda () (color-theme-solarized-light)))
+(add-hook 'after-make-frame-functions
+	  (lambda (frame)
+	    (reasonable-settings)
+	    (setq color-theme-is-global nil)
+	    (select-frame frame)
+	    (set-variable 'solarized-contrast 'normal)
+	    (setq solarized-termcolors 256)
+	    (if window-system
+		(color-theme-solarized-light)
+	      (color-theme-solarized-light))))
 
 (setq focus-follows-mouse t)
 (setq set-mark-command-repeat-pop t)
