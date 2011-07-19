799055e43d62c57a09eb159b4d9c50734263f792
Matthew Peddie <mpeddie@gmail.com>
2011-07-18 23:44:02 -0700

    Brought magit in as a submodule and updated lisp to reflect new path

--- libs/my-git.el
+++ libs/my-git.el
@@ -1,23 +1,7 @@
 ;;;;;;; git configuration ;;;;;;;;;
 
-; git
-(add-to-list 'load-path "/usr/share/doc/git-core/contrib/emacs/")
-(autoload 'git "git" "excellent version control system, emacs interface" t)
-(autoload 'vc-git "vc-git" "version control interface for git repos" t)
-(autoload 'git-blame "git-blame" "interface to git-blame" t)
-
-; egit
-(add-to-list 'load-path (concat my-home "software/egit"))
-(autoload 'egit "egit" "Emacs git history" t)
-(autoload 'egit-file "egit" "Emacs git history file" t)
-(autoload 'egit-dir "egit" "Emacs git history directory" t)
-
-; egg
-(add-to-list 'load-path (concat my-home "software/egg"))
-(autoload 'egg "egg" "Enhanced emacs git interface" t)
-(autoload 'egg-grep "egg-grep" "Enhanced emacs git interface -- grep" t)
-
 ; magit
+(local-path "site-lisp/magit")
 (require 'magit)
 (global-set-key (kbd "C-c s") 'magit-status)
 
