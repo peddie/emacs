2dc82bb7d7c8463c66037a1df5e60823a8f54e22
Matthew Peddie <peddie@alum.mit.edu>
2011-09-07 14:33:05 -0700

    add matlab mode repo

--- languages/my-matlab.el
+++ languages/my-matlab.el
@@ -1,18 +1,26 @@
 ;;;;;;;; MATLAB, that steaming pile of hoe ;;;;;;;;
 
-(local-path "site-lisp/matlab-mode")
+(local-path "site-lisp/matlab-emacs")
+
+(load-library "matlab-load")
 
 (autoload 'matlab-mode 
   (concat emacs-root "site-lisp/matlab-mode/matlab.el") 
   "Enter Matlab mode." t)
+
+; prefer this to octave-mode or mercury-mode -- it is better for
+; editing than octave-mode, and I write MATLAB more often than
+; Mercury.
+(assq-delete-all "\\.m\\'" auto-mode-alist)
 (setq auto-mode-alist 
       (cons '("\\.m\\'" . matlab-mode) auto-mode-alist))
 (autoload 'matlab-shell 
   (concat emacs-root "site-lisp/matlab-mode/matlab.el") 
   "Interactive Matlab mode." t)
+
 (defun my-matlab-mode-hook ()
   ; (setq matlab-indent-function t)       ; if you want function bodies indented
-  (setq fill-column 76)         ; where auto-fill should wrap
+  (setq fill-column 80)         ; where auto-fill should wrap
   ; (matlab-mode-hilit)
   (turn-on-auto-fill))
 (setq matlab-mode-hook 'my-matlab-mode-hook)
